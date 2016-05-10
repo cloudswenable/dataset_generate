6cf1428ebebdd5176998d23b77bb75ecb71611a7
Jimmy Sun <sunyongjie0404@163.com>
2016-05-10 22:30:43 +0800

    refine code for use the argument parse

--- tools/extract.py
+++ tools/extract.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 
-
 import sys
-
 import os
+import argparse
 
-
-def process():
-    file=open(sys.argv[1])
+def process(fname):
+    file = open(fname)
     ofile = open("out.txt", "w")
     rowid = 1
     for line in file:
@@ -23,4 +21,8 @@ def process():
 
 
 if __name__ == "__main__":
-    process()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f','--file', help='input file path for process', required=True, dest="file")
+    args = parser.parse_args()
+    fname = args.file
+    process(fname)
